84b079f2dde7969d9e78988c9874733fa46fbc43
jboehm1 <boehm.jean@gmail.com>
2023-07-13 20:35:35 +0200

    fiirst draft

--- test.py
+++ test.py
@@ -7,18 +7,22 @@ img = cv2.imread("C:/Users/Jean/Pictures/Capture.PNG")
 #img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 fast = cv2.FastFeatureDetector_create()
 
-kp = fast.detect(img,None)
-# Disable nonmaxSuppression
-fast.setNonmaxSuppression(True)
-fast.setThreshold(2)
 
-cv2.drawKeypoints(img, kp, img, color=(255,255,255))
+cv2.VideoCapture()
 
-print ("Threshold: ", fast.getThreshold())
-#print ("nonmaxSuppression: ", fast.getBool('nonmaxSuppression'))
-#print ("neighborhood: ", fast.getInt('type'))
-print ("Total Keypoints with nonmaxSuppression: ", len(kp))
+while True:
+    kp = fast.detect(img,None)
+    # Disable nonmaxSuppression
+    fast.setNonmaxSuppression(True)
+    fast.setThreshold(2)
 
-cv2.imwrite('fast_true_2.png',img)
+    cv2.drawKeypoints(img, kp, img, color=(255,255,255))
 
-cv2.waitKey()
+    print ("Threshold: ", fast.getThreshold())
+    #print ("nonmaxSuppression: ", fast.getBool('nonmaxSuppression'))
+    #print ("neighborhood: ", fast.getInt('type'))
+    print ("Total Keypoints with nonmaxSuppression: ", len(kp))
+
+    cv2.imwrite('fast_true_2.png',img)
+
+    cv2.waitKey()
